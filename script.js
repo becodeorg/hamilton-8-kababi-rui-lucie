196b2b96457684d2e5e3341dd59f24fd29df623f
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-06 16:06:09 +0200

    avancement

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const hamburger = document.querySelector(".icon_hamburger");
 const navMenu = document.querySelector(".topnav_menu");
 hamburger.addEventListener("click", () => {
     navMenu.classList.toggle("active");
-    hamburger.classList.toggle("opacity");
 })
 
 
