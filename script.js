31fc20c7f51b29cba7f1bf0bd75df51f94f6a8d9
[Lucie Monteiro] <[lucie.mtro@gmail.com]>
2023-06-06 20:40:47 +0200

    suppression ancien contenu de la  branche main

--- script.js
+++
@@ -1,8 +0,0 @@
-const hamburger = document.querySelector(".icon_hamburger");
-const navMenu = document.querySelector(".topnav_menu");
-hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    hamburger.classList.toggle("opacity");
-})
-
-
